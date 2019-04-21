af0ab6bbae93a8511baf1f26e9aa402b554443bd
AJMac <ajmac@ajmacs-imac.local>
2019-04-21 11:36:14 +0100

    Initial Commit

---
+++ Photo Frame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Frame
+//
+//  Created by AJMac on 4/21/19.
+//  Copyright © 2019 AJMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
